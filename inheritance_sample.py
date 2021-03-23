d9e4e471e00bb68ee92918d5aa5b4ac75e4ed3f3
Adarsh Liju <adarsh.liju.abraham@gmail.com>
2021-03-23 16:55:18 +0530

    Updated

--- inheritance_sample.py
+++ inheritance_sample.py
@@ -1,16 +1,20 @@
 '''
-    2 Inheritance
-    3 
-    4 Syntax:
-    5 class Base(object)
-    6     Base_Body
-    7 Derived(Base)
-    8     Derived_Body
-    9 
-   10 '''
-   11 class Parent:
-   12     pass
-   13 class Son(Parent):
-   14     pass
-   15 issubclass(Son,Parent) # issubclass(Derived_Class,Base_Class) : returns True
+Inheritance
+
+Syntax:
+class Base(object)
+    Base_Body
+Derived(Base)
+    Derived_Body
+
+'''
+#Example 1
+class Parent:
+    pass
+class Son(Parent):
+    pass
+issubclass(Son,Parent) # issubclass(Derived_Class,Base_Class) : returns True
+ 
+#Example 2
+class 
 
