dc9d8eb1685012e3b7bfc22c1cd6eaf1ebb84175
Adarsh Liju <adarsh.liju.abraham@gmail.com>
2021-03-23 12:17:55 +0530

    Added Inheritance example
    Signed-off-by: Adarsh Liju <adarsh.liju.abraham@gmail.com>

---
+++ inheritance_sample.py
@@ -0,0 +1,16 @@
+'''
+    2 Inheritance
+    3 
+    4 Syntax:
+    5 class Base(object)
+    6     Base_Body
+    7 Derived(Base)
+    8     Derived_Body
+    9 
+   10 '''
+   11 class Parent:
+   12     pass
+   13 class Son(Parent):
+   14     pass
+   15 issubclass(Son,Parent) # issubclass(Derived_Class,Base_Class) : returns True
+
