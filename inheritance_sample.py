9da98ac2e8d1c50d35952461c24535b40a35847e
Adarsh Liju <adarsh.liju.abraham@gmail.com>
2021-03-23 17:34:27 +0530

    Updated with another example

--- inheritance_sample.py
+++ inheritance_sample.py
@@ -8,13 +8,30 @@ Derived(Base)
     Derived_Body
 
 '''
+
 #Example 1
 class Parent:
     pass
 class Son(Parent):
     pass
 issubclass(Son,Parent) # issubclass(Derived_Class,Base_Class) : returns True
- 
+
 #Example 2
-class 
+class Debian:
+    def rel_date1(self):
+        print("Debian was released on 1993")
+class Ubuntu(Debian):# inheriting Debian class
+    def rel_date2(self):
+        print("Ubuntu was released on 2002")
+
+a=Ubuntu() # calling derived class Ubuntu
+a.rel_date1()
+a.rel_date2()
+'''
+When we create an object of the derived class,
+we invoke the constructor of the 
+derived class which in turn calls
+the base class constructor on the base class sub object.
+'''
+
 
